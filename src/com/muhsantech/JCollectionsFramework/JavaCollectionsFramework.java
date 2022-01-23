43d20af5e874021882864c48092baa2aa4d7bb04
MuhsanJaved <muhsantech048@outlook.com>
2022-01-23 10:17:09 +0500

    first commit

---
+++ src/com/muhsantech/JCollectionsFramework/JavaCollectionsFramework.java
@@ -0,0 +1,27 @@
+package com.muhsantech.JCollectionsFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaCollectionsFramework {
+    public static void main(String[] args) {
+
+        /*
+        String[] studentsName = new String[10];
+        studentsName[0] = "Ali ";
+//        studentsName[1]...studentsName[9]
+        studentsName[10] = "Saba khan";
+        //new Student
+        studentsName[11] = "Raman";
+        */
+
+//        ArrayList<String> studentsName = new ArrayList<>();
+//
+////        size = n
+////                n +n/2 +1
+//        studentsName.add("Muhsan");
+
+
+
+    }
+}
---
+++ src/com/muhsantech/Main.java
@@ -0,0 +1,8 @@
+package com.muhsantech;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
